4ad3b366d24a762ae74b0c0dd57ddd6b059d80ec
Ritesh <riteshkashyap9999@gmail.com>
2024-10-20 15:04:26 +0530

    loginPage changes

--- tests/thirdBridge/AddCompany.test.ts
+++ tests/thirdBridge/AddCompany.test.ts
@@ -1,20 +1,18 @@
 import test from '@lib/BaseTest';
 import { expect } from '@playwright/test';
+import { LoginPage } from '@pages/LoginPage';
 
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+test('has title', async ({ page , loginPage}) => {
+  await page.goto('https://onexweb-uat.officenational.co.za/');
 
   // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+  // await expect(page).toHaveTitle(/Playwright/);
+  // page.waitFor;
+
+  await page.waitForLoadState("load");
+  await loginPage.login();
+  
 });
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
 
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
-});
