09bf0bc05c50a3a9299c81c6b0465b50c31051c2
jesmijose19 <jesmijose19@gmail.com>
2024-10-23 18:47:18 +0530

    jesmi push

--- lib/BaseTest.ts
+++ lib/BaseTest.ts
@@ -1,61 +1,70 @@
-import { test as baseTest, Page, Browser, BrowserContext } from '@playwright/test';
+import {
+  test as baseTest,
+  Page,
+  Browser,
+  BrowserContext,
+} from '@playwright/test'
 
-import * as dotenv from 'dotenv';
+import * as dotenv from 'dotenv'
 
-import { VisualRegression } from '@pages/VisualRegression';
-import { Click } from '@pages/Click';
-import { Actions } from '@pages/Actions';
-dotenv.config();
+import { VisualRegression } from '@pages/VisualRegression'
+import { Click } from '@pages/Click'
+import { Actions } from '@pages/Actions'
+import { Verify } from '@pages/Verify'
+dotenv.config()
 
 const caps = {
-    'build': 'playwright-build-' + Math.floor(Math.random() * 300),
-    'browserstack.username': process.env.BROWSERSTACK_USERNAME,
-    'browserstack.accessKey': process.env.BROWSERSTACK_ACCESS_KEY,
-    'browser': process.env.BROWSER_NAME,
-    'os': process.env.OS,
-    'os_version': process.env.OS_VERSION,
-
-};
+  build: 'playwright-build-' + Math.floor(Math.random() * 300),
+  'browserstack.username': process.env.BROWSERSTACK_USERNAME,
+  'browserstack.accessKey': process.env.BROWSERSTACK_ACCESS_KEY,
+  browser: process.env.BROWSER_NAME,
+  os: process.env.OS,
+  os_version: process.env.OS_VERSION,
+}
 
 const test = baseTest.extend<{
-
-    visualRegression: VisualRegression;
-    Click: Click;
-    Actions: Actions;
+  visualRegression: VisualRegression
+  Click: Click
+  Actions: Actions
+  Verify: Verify
 }>({
+  page: async ({ playwright }, use, testInfo) => {
+    let browser: Browser
+    let context: BrowserContext
+    let page: Page
 
-    page: async ({ playwright }, use, testInfo) => {
-        let browser: Browser;
-        let context: BrowserContext;
-        let page: Page;
+    if (process.env.RUN_ON_BROWSERSTACK === 'true') {
+      browser = await playwright.chromium.connect({
+        wsEndpoint: `wss://cdp.browserstack.com/playwright?caps=${encodeURIComponent(
+          JSON.stringify(caps),
+        )}`,
+      })
+      context = await browser.newContext(testInfo.project.use)
+    } else {
+      browser = await playwright.chromium.launch()
+      context = await browser.newContext(testInfo.project.use)
+    }
 
-        if (process.env.RUN_ON_BROWSERSTACK === 'true') {
-            browser = await playwright.chromium.connect({
-                wsEndpoint: `wss://cdp.browserstack.com/playwright?caps=${encodeURIComponent(JSON.stringify(caps))}`,
-            });
-            context = await browser.newContext(testInfo.project.use);
-        } else {
-            browser = await playwright.chromium.launch();
-            context = await browser.newContext(testInfo.project.use);
-        }
+    page = await context.newPage()
+    await use(page)
 
-        page = await context.newPage();
-        await use(page);
+    await page.close()
+    await context.close()
+    await browser.close()
+  },
 
-        await page.close();
-        await context.close();
-        await browser.close();
-    },
-
-    visualRegression: async ({ page, context }, use) => {
-        await use(new VisualRegression(page, context));
-    },
-    Click: async ({ page, context }, use) => {
-        await use(new Click(page, context));
-    },
-    Actions: async ({ page, context }, use) => {
-        await use(new Actions(page, context));
-    },
+  visualRegression: async ({ page, context }, use) => {
+    await use(new VisualRegression(page, context))
+  },
+  Click: async ({ page, context }, use) => {
+    await use(new Click(page, context))
+  },
+  Actions: async ({ page, context }, use) => {
+    await use(new Actions(page, context))
+  },
+  Verify: async ({ page, context }, use) => {
+    await use(new Verify(page, context))
+  },
 })
 
-export default test;
+export default test
--- tests/thirdBridge/AddCompany.test.ts
+++ tests/thirdBridge/AddCompany.test.ts
@@ -1,21 +1,17 @@
-import test from '@lib/BaseTest';
-import { expect } from '@playwright/test';
-import { Actions } from '@pages/Actions';
-import { Click } from '@pages/Click';
+import test from '@lib/BaseTest'
+import { expect } from '@playwright/test'
+import { Actions } from '@pages/Actions'
+import { Click } from '@pages/Click'
+import { Verify } from '@pages/Verify'
 
-
-test('has title', async ({ page , Actions , Click}) => {
-  await page.goto('https://onexweb-uat.officenational.co.za/');
-
-  // Expect a title "to contain" a substring.
-  // await expect(page).toHaveTitle(/Playwright/);
-  // page.waitFor;
-
-  await page.waitForLoadState("load");
-  await Actions.enterText("email","jeigemmabrije-7589@yopmail.com");
-  await Actions.enterText("password","Testing@1212");
-  await Click.Btn("login");
-});
-
-
-
+test('has title', async ({ page, Actions, Click, Verify }) => {
+  await page.goto('https://onexweb-uat.officenational.co.za/')
+  await Actions.enterText('email', 'jeigemmabrije-7589@yopmail.com')
+  await Actions.enterText('password', 'Testing@1212')
+  await Click.Btn('login')
+  await Verify.IsTextDisplayed('Sign in')
+})
+test('jesmi testcase', async ({ page, Actions, Click, Verify }) => {
+  await page.goto('https://onexweb-uat.officenational.co.za/')
+  await Verify.IsTextDisplayed('Sign in')
+})
