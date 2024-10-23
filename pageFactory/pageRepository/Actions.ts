7920614208fb9fb54282fc124e018c0ed6e485c0
Ritesh <riteshkashyap9999@gmail.com>
2024-10-23 17:59:17 +0530

    Click Tab Funcation

--- pageFactory/pageRepository/Actions.ts
+++ pageFactory/pageRepository/Actions.ts
@@ -32,5 +32,7 @@ export class Actions {
         }
     }
 
+    
+
 
 } 
--- tests/thirdBridge/AddCompany.test.ts
+++ tests/thirdBridge/AddCompany.test.ts
@@ -4,7 +4,7 @@ import { Actions } from '@pages/Actions';
 import { Click } from '@pages/Click';
 
 
-test('has title', async ({ page , Actions , Click}) => {
+test('has title', async ({ page, Actions, Click }) => {
   await page.goto('https://onexweb-uat.officenational.co.za/');
 
   // Expect a title "to contain" a substring.
@@ -12,9 +12,11 @@ test('has title', async ({ page , Actions , Click}) => {
   // page.waitFor;
 
   await page.waitForLoadState("load");
-  await Actions.enterText("email","jeigemmabrije-7589@yopmail.com");
-  await Actions.enterText("password","Testing@1212");
+  await Actions.enterText("email", "jeigemmabrije-7589@yopmail.com");
+  await Actions.enterText("password", "Testing@1212");
   await Click.Btn("login");
+
+  await Click.tabs("Suppliers");
 });
 
 
