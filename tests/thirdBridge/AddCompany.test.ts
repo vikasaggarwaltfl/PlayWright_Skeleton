5d0f806b3e12a5401221299a54382899a0ab50cb
Ritesh <85149732+Ritesh1919@users.noreply.github.com>
2024-10-26 11:18:15 +0530

    Update AddCompany.test.ts

--- tests/thirdBridge/AddCompany.test.ts
+++ tests/thirdBridge/AddCompany.test.ts
@@ -39,37 +39,6 @@ import { Click } from '@pages/Click'
  });
 
 
-// test('ritesh TC_ID_013', async ({ page, Actions, Click }) => {
-//   await page.goto('https://onexweb-uat.officenational.co.za/');
-
-//   await Actions.enterText("email", "jeigemmabrije-7589@yopmail.com");
-//   await Actions.enterText("password", "Testing@1212");
-//   await Click.Btn("login");
-
-//   await Click.Btn("ProfileBtn");
-// });
-
-// test('ritesh TC_ID_014', async ({ page, Actions, Click }) => {
-//   await page.goto('https://onexweb-uat.officenational.co.za/');
-
-//   await Actions.enterText("email", "jeigemmabrije-7589@yopmail.com");
-//   await Actions.enterText("password", "Testing@1212");
-//   await Click.Btn("login");
-
-//   await Click.tabs("ProductsTab");
-// });
-
-// test('ritesh TC_ID_015', async ({ page, Actions, Click }) => {
-//   await page.goto('https://onexweb-uat.officenational.co.za/');
-
-//   await Actions.enterText("email", "jeigemmabrije-7589@yopmail.com");
-//   await Actions.enterText("password", "Testing@1212");
-//   await Click.Btn("login");
-
-//   await Click.icon("MinimizeMenuBtn");
-// });
-
-
 
 test('ritesh TC_ID_007', async ({ page, Actions, Click }) => {
   await page.goto('https://onexweb-uat.officenational.co.za/');
