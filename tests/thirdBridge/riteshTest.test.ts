778367f858c0d0a6ca9312ac7329a71494cef2db
Ritesh <riteshkashyap9999@gmail.com>
2024-11-02 11:20:06 +0530

    Ritesh 2/11/24 morning

--- tests/thirdBridge/riteshTest.test.ts
+++ tests/thirdBridge/riteshTest.test.ts
@@ -2,36 +2,22 @@ import test from '@lib/BaseTest'
 import { expect } from '@playwright/test'
 import { Actions } from '@pages/Actions'
 import { Click } from '@pages/Click'
-import { verify } from 'crypto';
-import { access } from 'fs';
 
 test('ritesh TC_ID_007', async ({ page, Actions, Click }) => {
-    await page.goto('https://onexweb-uat.officenational.co.za/');
-
-    await Actions.enterText("email", "jeigemmabrije-7589@yopmail.com");
-    await Actions.enterText("password", "Testing@1212");
+    await Actions.signIn();
     await Click.Btn("login");
-
     await Click.Btn("ProfileBtn");
 });
 
 test('ritesh TC_ID_008', async ({ page, Actions, Click }) => {
-    await page.goto('https://onexweb-uat.officenational.co.za/');
-
-    await Actions.enterText("email", "jeigemmabrije-7589@yopmail.com");
-    await Actions.enterText("password", "Testing@1212");
+    await Actions.signIn();
     await Click.Btn("login");
-
     await Click.tabs("ProductsTab");
 });
 
 test('ritesh TC_ID_009', async ({ page, Actions, Click }) => {
-    await page.goto('https://onexweb-uat.officenational.co.za/');
-
-    await Actions.enterText("email", "jeigemmabrije-7589@yopmail.com");
-    await Actions.enterText("password", "Testing@1212");
+    await Actions.signIn();
     await Click.Btn("login");
-
     await Click.icon("MinimizeMenuBtn");
 });
 
