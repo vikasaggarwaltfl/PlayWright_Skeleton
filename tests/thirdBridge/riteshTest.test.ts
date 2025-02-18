8bb3bf4c39b021b674bab398a3f5168f6441d77f
sonali-98 <aadekarsonali12@gmail.com>
2025-02-18 18:03:42 +0530

    push from sonali 6

--- tests/thirdBridge/riteshTest.test.ts
+++ tests/thirdBridge/riteshTest.test.ts
@@ -5,18 +5,18 @@ import { Click } from '@pages/Click'
 import { verify } from 'crypto';
 import { TIMEOUT } from 'dns';
 
-test('ritesh TC_ID_007', async ({ page, Actions, Click }) => {
-    await Actions.signIn();
-    await Click.Btn("login");
-    await page.waitForTimeout(1000);
-    await Click.Btn("ProductTab");
-    await page.waitForTimeout(2000);
-    await Click.Btn("ProductWIP");
-    await page.waitForTimeout(2000);
-    await Click.Btn("ProfileBtn");
-    await page.waitForTimeout(2000);
-    await Click.Btn("SignoutBtn");
-});
+// test('ritesh TC_ID_007', async ({ page, Actions, Click }) => {
+//     await Actions.signIn();
+//     await Click.Btn("login");
+//     await page.waitForTimeout(1000);
+//     await page.waitForTimeout(2000);
+//     // await Click.Btn("ProductTab");
+//     // await Click.Btn("ProductWIP");
+//     // await page.waitForTimeout(2000);
+//     // await Click.Btn("ProfileBtn");
+//     // await page.waitForTimeout(2000);
+//     // await Click.Btn("SignoutBtn");
+// });
 
 
 // test('ritesh TC_ID_008', async ({ page, Actions, Click }) => {
@@ -89,47 +89,32 @@ test('ritesh TC_ID_007', async ({ page, Actions, Click }) => {
 //     // await Actions.logout();
 // });
 
-test('Add Master Product Category Setup and verify the data', async ({ Actions, Click, Verify, page }) => {
-    // await page.setViewportSize({ width: 1920, height: 1080 });
-    await Actions.signIn();
-    await Click.Btn("login");
-    await Click.tabs("GroupSettingsTab")
-    await Click.link("MasterProductCategorySetup");
-    await Click.link("MasterProductCategorySetup");
-    await Click.Btn("addProdcutCatogerySetup");
-    await Actions.enterText("Category_Code", "00060");
-    await Actions.enterText("Category", "testingjjj");
-    await Actions.enterText("Category_Code", "00060");
-    await Actions.enterText("Category", "testingjjj");
-    await Click.Btn("saveProdcutCatogerySetup");
-    await page.locator("//button[@class='p-toast-icon-close p-link']//*[name()='svg']").click();
-    await page.locator("//button[@class='p-toast-icon-close p-link']//*[name()='svg']").waitFor({ state: 'detached' });
-    await page.getByLabel('Last Page').click();
-    await page.locator("tbody tr:nth-child(1)").waitFor({ state: 'attached' });
-    await page.waitForTimeout(3000); 
-    await page.locator("//button[@class='p-toast-icon-close p-link']//*[name()='svg']").click();
-    await page.locator("//button[@class='p-toast-icon-close p-link']//*[name()='svg']").waitFor({ state: 'detached' });
-    await page.getByLabel('Last Page').click();
-    await page.locator("tbody tr:nth-child(1)").waitFor({ state: 'attached' });
-    await page.waitForTimeout(3000); 
-    await Actions.dataAssertion("MasterProductCategorySetup");
-    // await page.pause();
-    // await page.pause();
-});
-
-
-// test('Checking the master prodcut catogery is created or not', async ({ Actions, Click, Verify, page }) => {
+// test('Add Master Product Category Setup and verify the data', async ({ Actions, Click, Verify, page }) => {
+//     // await page.setViewportSize({ width: 1920, height: 1080 });
 //     await Actions.signIn();
 //     await Click.Btn("login");
 //     await Click.tabs("GroupSettingsTab")
-//     await Click.link("MasterProductCategorySetup")
-//     // await page.locator("//nav[@class='p-paginator-bottom']").waitFor();
-//     await page.waitForLoadState();
-//     // await page.locator("//table").waitFor();
-//     // await Click.Btn("paginatorToLast");
-//     // await test.setTimeout(2000);
+//     await Click.link("MasterProductCategorySetup");
+//     await Click.link("MasterProductCategorySetup");
+//     await Click.Btn("addProdcutCatogerySetup");
+//     await Actions.enterText("Category_Code", "00060");
+//     await Actions.enterText("Category", "testingjjj");
+//     await Actions.enterText("Category_Code", "00060");
+//     await Actions.enterText("Category", "testingjjj");
+//     await Click.Btn("saveProdcutCatogerySetup");
+//     await page.locator("//button[@class='p-toast-icon-close p-link']//*[name()='svg']").click();
+//     await page.locator("//button[@class='p-toast-icon-close p-link']//*[name()='svg']").waitFor({ state: 'detached' });
+//     await page.getByLabel('Last Page').click();
+//     await page.locator("tbody tr:nth-child(1)").waitFor({ state: 'attached' });
+//     await page.waitForTimeout(3000); 
+//     await page.locator("//button[@class='p-toast-icon-close p-link']//*[name()='svg']").click();
+//     await page.locator("//button[@class='p-toast-icon-close p-link']//*[name()='svg']").waitFor({ state: 'detached' });
+//     await page.getByLabel('Last Page').click();
+//     await page.locator("tbody tr:nth-child(1)").waitFor({ state: 'attached' });
+//     await page.waitForTimeout(3000); 
 //     await Actions.dataAssertion("MasterProductCategorySetup");
-
+//     // await page.pause();
+//     // await page.pause();
 // });
 
 
@@ -147,18 +132,33 @@ test('Add Master Product Category Setup and verify the data', async ({ Actions, 
 
 // });
 
-test('uploading files', async ({ Actions, Click, Verify, page }) => {
-    await Actions.signIn();
-    await Click.Btn("login");
-    await Click.tabs("ProductsTab");    
-    await page.waitForLoadState();
-    await Click.link("ProductMedia");
-    await page.waitForTimeout(2000);
-    await Click.tabs("ProductsTab");    
-    await page.waitForLoadState();
-    await Click.link("ProductMedia");
-    await page.waitForTimeout(2000);
-    await Click.link("productMediaCode");
-    await Actions.uploadFile("//button[@aria-label='Add File']", "ProductPage");
-    await Actions.uploadFile("//button[@aria-label='Add File']", "ProductPage");
-});
+
+// test('Checking the master prodcut catogery is created or not', async ({ Actions, Click, Verify, page }) => {
+//     await Actions.signIn();
+//     await Click.Btn("login");
+//     await Click.tabs("GroupSettingsTab")
+//     await Click.link("MasterProductCategorySetup")
+//     // await page.locator("//nav[@class='p-paginator-bottom']").waitFor();
+//     await page.waitForLoadState();
+//     // await page.locator("//table").waitFor();
+//     // await Click.Btn("paginatorToLast");
+//     // await test.setTimeout(2000);
+//     await Actions.dataAssertion("MasterProductCategorySetup");
+
+// });
+
+// test('uploading files', async ({ Actions, Click, Verify, page }) => {
+//     await Actions.signIn();
+//     await Click.Btn("login");
+//     await Click.tabs("ProductsTab");    
+//     await page.waitForLoadState();
+//     await Click.link("ProductMedia");
+//     await page.waitForTimeout(2000);
+//     await Click.tabs("ProductsTab");    
+//     await page.waitForLoadState();
+//     await Click.link("ProductMedia");
+//     await page.waitForTimeout(2000);
+//     await Click.link("productMediaCode");
+//     await Actions.uploadFile("//button[@aria-label='Add File']", "ProductPage");
+//     await Actions.uploadFile("//button[@aria-label='Add File']", "ProductPage");
+// });
--- tests/thirdBridge/sonaliTest.test.ts
+++ tests/thirdBridge/sonaliTest.test.ts
@@ -1,3 +1,4 @@
+
 import test from '@lib/BaseTest'
 import { expect } from '@playwright/test'
 import { Actions } from '@pages/Actions'
@@ -6,37 +7,57 @@ import { verify } from 'crypto'
 import { access } from 'fs'
 
 
-test('end to end test_supplier', async ({ page, Actions, Click }) => {
-
-    // await page.goto('https://onexweb-uat.officenational.co.za/');
-    // await Actions.enterText("email", "jeigemmabrije-7589@yopmail.com");
-    // await Actions.enterText("password", "Testing@1212");
-    // await Click.Btn("login");
-
+test('user login', async ({ page, Actions, Click }) => {
     await Actions.signIn();
     await Click.Btn("login");
-    await Actions.enterText("searchmenu", "Suppliers")
-    await Click.tabs("SuppliersTab")
-    await Click.icon("FilterDropDown")
-    await Actions.enterText("nameFilter", "Sofiya")
-
 });
 
-test('end to end test_New Brand ', async ({ Actions, Click, Verify, page }) => {
-
+test.only('Add New Brand ', async ({ Actions, Click, Verify, page }) => {
     await Actions.signIn();
     await Click.Btn("login");
     await Click.tabs("BrandsTab")
-    await Click.link("NewBrand")
+    await Click.link("AddnewBrand")
+    await Actions.enterText("enterBrandName","TestBrand2");
+    await Click.Btn("SavenewBrand");
     await page.pause();
-    await Actions.enterText("enterBrandName","TestingBrand1");
-    await Click.Btn("BrandDropBox");
-    await Actions.uploadFile("//div[text()='Drop files here to upload logo']","NewBrand");
 })
 
 
 
-     //await Actions.scrollPage('down', 500); // Scroll down 500 pixels
-    //  await Actions.scrollPage('up', 200); // Scroll up 200 pixels
-    //  await Actions.scrollPage('right', 300); // Scroll right 300 pixels
-    //  await Actions.scrollPage('left', 100); // Scroll left 100 pixels
+
+
+
+
+
+
+
+// test.('user login', async ({ page, Actions, Click }) => {
+
+//     await page.goto('https://onexweb-uat.officenational.co.za/');
+//     await Actions.enterText("email", "jeigemmabrije-7589@yopmail.com");
+//     await Actions.enterText("password", "Testing@1212");
+//     await Click.Btn("login");
+//     await Actions.signIn();
+//     await Click.Btn("login");
+//     await Actions.enterText("searchmenu", "Suppliers")
+//     await Click.tabs("SuppliersTab")
+//     await Click.icon("FilterDropDown")
+//     await Actions.enterText("nameFilter", "Sofiya")
+
+// });
+
+// test('Add New Brand ', async ({ Actions, Click, Verify, page }) => {
+
+//     await Actions.signIn();
+//     await Click.Btn("login");
+//     await Click.tabs("BrandsTab")
+//     await Click.link("NewBrand")
+//     await page.pause();
+//     await Actions.enterText("enterBrandName","TestingBrand1");
+//     await Click.Btn("BrandDropBox");
+//     await Actions.uploadFile("//div[text()='Drop files here to upload logo']","NewBrand");
+// })
+
+
+
+     
