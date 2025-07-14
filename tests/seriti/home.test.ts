8edb2fc3f6e4f1739265f595682862e1f209db6d
sonali-98 <aadekarsonali12@gmail.com>
2025-07-14 11:33:48 +0530

    Fixed home.test.ts file

--- tests/seriti/home.test.ts
+++ tests/seriti/home.test.ts
@@ -1,20 +1,16 @@
-import test from '@lib/BaseTest'
-import { expect } from '@playwright/test'
-import { Actions } from '@pages/Actions'
-import { Click } from '@pages/Click'
-import { Verify } from 'crypto'
-import { access } from 'fs'
+import test from '@lib/BaseTest';
+import { expect } from '@playwright/test';
+import { Actions } from '@pages/Actions';
+import { Click } from '@pages/Click';
+import { Verify } from '@pages/Verify';
 
-
-
-// Home-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-
-test('Verify that Home sidebar option displayed correctly with icon', async ({ page, Actions, Click, Verify }) => {
+// Home Sidebar Option Test -----------------------------------------------------------------------------
+test('Verify that Home sidebar option is displayed correctly with icon', async ({ page, Actions, Click, Verify }) => {
     await Actions.signIn("sonali");
     await Click.Btn("login");   
     await Verify.IsTextDisplayed(page, "Home");
     await Verify.isIconVisible(page, 'homeIcon');
-    await console.log ("The Home sidebar option is displayed as expected with the icon.")
+    await console.log("The Home sidebar option is displayed as expected with the icon.");
 });
 
-// Home page is still not fully developed.
+// Note: Home page is still not fully developed.
