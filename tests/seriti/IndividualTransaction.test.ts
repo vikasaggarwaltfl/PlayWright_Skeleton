dae74b02436aa6572d140bf864eab3696152c423
sonali-98 <aadekarsonali12@gmail.com>
2025-08-05 12:47:12 +0530

    Individual Trans test cases

---
+++ tests/seriti/IndividualTransaction.test.ts
@@ -0,0 +1,105 @@
+
+import test from '@lib/BaseTest';
+import { expect } from '@playwright/test';
+import { Actions } from '@pages/Actions';
+import { Click } from '@pages/Click';
+
+
+test('Verify that a user can initiate the individual create transaction process with valid input', async ({ page, Actions, Click, Verify }) => {
+    await Actions.signIn("sonali");
+    await Click.Btn("login");
+    await Click.Btn("createTransaction");
+    await Click.dropdown("Select a group", "Test_Group")
+    await Click.dropdown("Select a branch", "Test_Branch")
+    await Click.radioButton("Individual");
+    await Actions.enterText("lastName", "TestTransaction");
+    await Click.Btn("clickTransaction");
+});
+
+test('Verify that the user is redirected to the transaction details screen after creating individual transaction. ', async ({ page, Actions, Click, Verify }) => {
+    await Actions.signIn("sonali");
+    await Click.Btn("login");
+    await Click.Btn("createTransaction");
+    await Click.dropdown("Select a group", "Test_Group")
+    await Click.dropdown("Select a branch", "Test_Branch")
+    await Click.radioButton("Individual");
+    await Actions.enterText("lastName", "TestTransaction");
+    await Click.Btn("clickTransaction");
+    await Verify.IsTextDisplayed(page, 'Quick Access');
+});
+
+test('Verify that navbar options are displayed as expected at the top of the screeen.', async ({ page, Actions, Click, Verify }) => {
+    await Actions.signIn("sonali");
+    await Click.Btn("login");
+    await Actions.enterText("transactionSearchMenu", "281913");
+    await Click.Btn("view");
+    await page.waitForLoadState('networkidle');
+    await Verify.IsTextDisplayed(page, ["Show Required", "Hide Overview", "Client Details", "Vehicle Details", "Account Details"]);
+});
+
+test('Verify that the Show Required tab displays only mandatory fields on the form.', async ({ page, Actions, Click, Verify }) => {
+    await Actions.signIn("sonali");
+    await Click.Btn("login");
+    await Actions.enterText("transactionSearchMenu", "281913");
+    await Click.Btn("view");
+    await page.waitForLoadState('networkidle');
+    await Click.transactionTabs("Show Required");
+    await Verify.IsTextDisplayed(page, ["Transaction Status", "Finance Status", "Transaction Status Notes"]);
+    await Click.transactionTabs("Vehicle Details");
+    await Verify.IsTextDisplayed(page, "Vehicle Condition");
+});
+
+test('Verify that the "Show All" tab displays all fields on the form.', async ({ page, Actions, Click, Verify }) => {
+    await Actions.signIn("sonali");
+    await Click.Btn("login");
+    await Actions.enterText("transactionSearchMenu", "281913");
+    await Click.Btn("view");
+    await page.waitForLoadState('networkidle');
+    await Verify.IsTextDisplayed(page, ["Preferred Contact Time", "Finance Status", "Fleet Number"]);
+});
+
+test('Verify that the Hide Overview tab hides the overview section of transaction.', async ({ page, Actions, Click, Verify }) => {
+    await Actions.signIn("sonali");
+    await Click.Btn("login");
+    await Actions.enterText("transactionSearchMenu", "281913");
+    await Click.Btn("view");
+    await page.waitForLoadState('networkidle');
+    await Click.transactionTabs("Hide Overview");
+    await expect(page.locator('//span[normalize-space()="TESTING COMPANY"]')).toBeHidden();
+});
+
+test('Verify that the Show Overview tab displays the overview section of transaction.', async ({ page, Actions, Click, Verify }) => {
+    await Actions.signIn("sonali");
+    await Click.Btn("login");
+    await Actions.enterText("transactionSearchMenu", "281913");
+    await Click.Btn("view");
+    await page.waitForLoadState('networkidle');
+    await Click.transactionTabs("Hide Overview");
+    await Click.transactionTabs("Show Overview");
+    await expect(page.locator('//span[normalize-space()="TESTING COMPANY"]')).toBeVisible();
+});
+
+test('Verify that all form sections are displayed sequentially on the screen', async ({ page, Actions, Click, Verify }) => {
+    await Actions.signIn("sonali");
+    await Click.Btn("login");
+    await Actions.enterText("transactionSearchMenu", "281913");
+    await Click.Btn("view");
+    await page.waitForLoadState('networkidle');
+    await Click.transactionTabs("Show Required", true);
+});
+
+test('Verify that the user can expand and collapse the details by clicking on the collapse button.', async ({ page, Actions, Click, Verify }) => {
+    await Actions.signIn("sonali");
+    await Click.Btn("login");
+    await Actions.enterText("transactionSearchMenu", "281913");
+    await Click.Btn("view");
+    await page.waitForLoadState('networkidle');
+    await Click.icon("expandAll");
+    await Click.icon("collapseAll");
+    await expect(page.locator('//span[normalize-space()="Change Vehicle"]')).toBeHidden();
+});
+
+
+
+
+
