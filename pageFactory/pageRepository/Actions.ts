1935f8f455c36ec52bc956c1e02addb5c0390c47
sonali-98 <aadekarsonali12@gmail.com>
2025-09-03 18:19:04 +0530

    Testing demo

--- pageFactory/pageRepository/Actions.ts
+++ pageFactory/pageRepository/Actions.ts
@@ -116,7 +116,7 @@ export class Actions {
         await this.page.goto('https://seritiweb-mea-uat.seriti-int.com');
         if (userProfile === "sonali") {
             await this.enterText("email", "sonali@testingframeworks.co.uk");
-            await this.enterText("password", "Nova@1234");
+            await this.enterText("password", "Password#123");
         }
         else if (userProfile === "Automation") {
             await this.enterText("email", "test-automation@testingframeworks.co.uk");
--- tests/seriti/myReports.test.ts
+++ tests/seriti/myReports.test.ts
@@ -26,7 +26,7 @@ test('Verify that the Deal Tracker Report screen is displayed as expected', asyn
     await Verify.IsTextDisplayed(page, ["Created Date", "Process State Message", "Report File"]);
 });
 
-test('Verify that the user can "Add" new Deal Tracker Report with valid data', async ({ page, Actions, Click, Verify }) => {
+test.only('Verify that the user can "Add" new Deal Tracker Report with valid data', async ({ page, Actions, Click, Verify }) => {
     await Actions.signIn("sonali");
     await Click.Btn("login");
     await Actions.enterText("searchMenu", "My Reports");
@@ -43,7 +43,7 @@ test('Verify that the user can "Add" new Deal Tracker Report with valid data', a
     await Verify.IsTextDisplayed(page, "Saved Successfully");
 });
 
-test('Verify that the user cannot "Add" new Deal Tracker Report with Invalid data', async ({ page, Actions, Click, Verify }) => {
+test.only('Verify that the user cannot "Add" new Deal Tracker Report with Invalid data', async ({ page, Actions, Click, Verify }) => {
     await Actions.signIn("sonali");
     await Click.Btn("login");
     await Actions.enterText("searchMenu", "My Reports");
