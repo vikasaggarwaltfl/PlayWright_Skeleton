160b0dd63cf6b9a57d8a4d3ac959a7bd2c904191
akshayaachu5169 <akshayaachu5169@gmail.com>
2025-09-03 18:07:52 +0100

    maximize screen

--- playwright.config.ts
+++ playwright.config.ts
@@ -39,7 +39,8 @@ export default defineConfig({
   projects: [
     {
       name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
+      use: { ...devices['Desktop Chrome'] , viewport: { width: 1920, height: 1080 } },
+      
     },
 
     // {
