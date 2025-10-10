5d07020c213f5e0a6277b690ed6bacc17075d703
sonali-98 <aadekarsonali12@gmail.com>
2025-10-10 18:11:30 +0530

    modified and fixed  tests for Auto Incept

--- tests/seriti/autoIncept.test.ts
+++ tests/seriti/autoIncept.test.ts
@@ -5,37 +5,31 @@ import { Click } from '@pages/Click'
 import { Verify } from '@pages/Verify'
 import * as path from 'path'
 
-test.only('Verify that Auto Incept display as expected', async ({ page, Actions, Click, Verify }) => {
+test('Verify that Auto Incept display as expected', async ({ page, Actions, Click, Verify }) => {
     await Actions.signIn("Automation");
     await Click.Btn("login");
     await Actions.enterText("searchMenu", "Auto-Incept");
     
 });
 
-test.only('Verify that Auto Incept filter working as expected', async ({ page, Actions, Click, Verify }) => {
+test('Verify that Auto Incept filter working as expected', async ({ page, Actions, Click, Verify }) => {
     await Actions.signIn("Automation");
     await Click.Btn("login");
     await Actions.enterText("searchMenu", "Auto-Incept");
 
 });
 
-test.only('Verify that user can Refresh Auto Incept table data', async ({ page, Actions, Click, Verify }) => {
-    await Actions.signIn("Automation");
-    await Click.Btn("login");
-    await Actions.enterText("searchMenu", "Auto-Incept");
-    
-});
-
-test.only('Verify that Auto Incept table data displyed correctly', async ({ page, Actions, Click, Verify }) => {
+test('Verify that user can Refresh Auto Incept table data', async ({ page, Actions, Click, Verify }) => {
     await Actions.signIn("Automation");
     await Click.Btn("login");
     await Actions.enterText("searchMenu", "Auto-Incept");
     
 });
 
-test.only('Verify that user can Refresh Auto Incept table data', async ({ page, Actions, Click, Verify }) => {
+test('Verify that Auto Incept table data displyed correctly', async ({ page, Actions, Click, Verify }) => {
     await Actions.signIn("Automation");
     await Click.Btn("login");
     await Actions.enterText("searchMenu", "Auto-Incept");
     
 });
+
